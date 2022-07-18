bdd5a403e433b513a1448908f0ffa37ca1de36cf
Anton Zhdanov <anton.s.zhdanov@gmail.com>
2022-07-18 14:04:01 +0300

    added a few "important" comments

--- src/main/java/net/illia/illiafabricmod1_19/IlliaFabricMod1_19.java
+++ src/main/java/net/illia/illiafabricmod1_19/IlliaFabricMod1_19.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//This Is The Main Mod Class :)
 public class IlliaFabricMod1_19 implements ModInitializer {
 	public static final String MOD_ID = "illiafabricmod1_19";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
--- src/main/java/net/illia/illiafabricmod1_19/mixin/ExampleMixin.java
+++ src/main/java/net/illia/illiafabricmod1_19/mixin/ExampleMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//This is A Mixin :)
 
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
