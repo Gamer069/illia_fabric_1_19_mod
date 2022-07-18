b544f995a4a48649b83313860e1c58926f25c508
Anton Zhdanov <anton.s.zhdanov@gmail.com>
2022-07-18 14:38:50 +0300

    added a few "important" comments

--- src/main/java/net/illia/illiafabricmod1_19/IlliaFabricMod1_19.java
+++ src/main/java/net/illia/illiafabricmod1_19/IlliaFabricMod1_19.java
@@ -1,6 +1,7 @@
 package net.illia.illiafabricmod1_19;
 
 import net.fabricmc.api.ModInitializer;
+import net.illia.illiafabricmod1_19.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class IlliaFabricMod1_19 implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
