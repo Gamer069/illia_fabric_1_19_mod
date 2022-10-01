b18baecbb20e161745fb22b2b9bfa460a4493f94
Illia Zhdanov <il.25.zhd@gmail.com>
2022-10-01 10:33:50 +0300

    idk

--- src/main/java/net/illia/illiafabricmod1_19/world/gen/ModTreeGeneration.java
+++ src/main/java/net/illia/illiafabricmod1_19/world/gen/ModTreeGeneration.java
@@ -3,11 +3,10 @@ package net.illia.illiafabricmod1_19.world.gen;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.illia.illiafabricmod1_19.world.feature.ModPlacedFeatures;
-import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.GenerationStep;
 
 public class ModTreeGeneration {
 	public static void genTrees() {
-		BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.PLAINS), GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.TESTWOOD_PLACED.getKey().get());
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.TESTWOOD_PLACED.getKey().get());
 	}
 }
